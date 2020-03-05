5558ca092709507d105a991b90d2e0c254ed2b84
Chase Jacobs <ChaseJacobs711@gmail.com>
2020-03-04 23:18:33 -0600

    change name of backup destination var

--- BackupScript.ps1
+++ BackupScript.ps1
@@ -8,7 +8,7 @@
 ########################################################
 
 #Description: Copies the Bakupdirs to the Destination
-#You can configure more than one Backupdirs, every Dir
+#You can configure more than one BackupDestinationdirs, every Dir
 #wil be copied to the Destination. A Progress Bar
 #is showing the Status of copied MB to the total MB
 #Only Change Variables in Variables Section
@@ -28,7 +28,7 @@ $Destination=$destination #Copy the Files to this Location
 $Staging=$stage
 $ClearStaging=$true # When $true, Staging Dir will be cleared
 $Versions="5" #How many of the last Backups you want to keep
-$BackupDirs=$source #What Folders you want to backup
+$BackupDestinationdirs=$source #What Folders you want to backup
 
 $ExcludeDirs="" #This list of Directories will not be copied
 
@@ -57,16 +57,16 @@ $ExcludeString=$ExcludeString.Substring(0,$ExcludeString.Length-1)
 if ($UseStaging -and $Zip)
 {
     #Logging "INFO" "Use Temp Backup Dir"
-    $Backupdir=$Staging +"\Backup-"+ (Get-Date -format yyyy-MM-dd)+"-"+(Get-Random -Maximum 100000)+"\"
+    $BackupDestinationdir=$Staging +"\Backup-"+ (Get-Date -format yyyy-MM-dd)+"-"+(Get-Random -Maximum 100000)+"\"
 }
 else
 {
     #Logging "INFO" "Use orig Backup Dir"
-    $Backupdir=$Destination +"\Backup-"+ (Get-Date -format yyyy-MM-dd)+"-"+(Get-Random -Maximum 100000)+"\"
+    $BackupDestinationdir=$Destination +"\Backup-"+ (Get-Date -format yyyy-MM-dd)+"-"+(Get-Random -Maximum 100000)+"\"
 }
 
-#$BackupdirTemp=$Temp +"\Backup-"+ (Get-Date -format yyyy-MM-dd)+"-"+(Get-Random -Maximum 100000)+"\"
-$Log=$Backupdir+$LogName
+#$BackupDestinationdirTemp=$Temp +"\Backup-"+ (Get-Date -format yyyy-MM-dd)+"-"+(Get-Random -Maximum 100000)+"\"
+$Log=$BackupDestinationdir+$LogName
 $Log
 $Items=0
 $Count=0
@@ -91,15 +91,15 @@ Function Logging ($State, $Message) {
 }
 
 
-#Create Backupdir
-Function Create-Backupdir {
-    New-Item -Path $Backupdir -ItemType Directory | Out-Null
+#Create BackupDestinationdir
+Function Create-BackupDestinationdir {
+    New-Item -Path $BackupDestinationdir -ItemType Directory | Out-Null
     sleep -Seconds 5
-    Logging "INFO" "Create Backupdir $Backupdir"
+    Logging "INFO" "Create BackupDestinationdir $BackupDestinationdir"
 }
 
-#Delete Backupdir
-Function Delete-Backupdir {
+#Delete BackupDestinationdir
+Function Delete-BackupDestinationdir {
     $Folder=Get-ChildItem $Destination | where {$_.Attributes -eq "Directory"} | Sort-Object -Property CreationTime -Descending:$false | Select-Object -First 1
 
     Logging "INFO" "Remove Dir: $Folder"
@@ -117,12 +117,12 @@ Function Delete-Zip {
     $Zip.FullName | Remove-Item -Recurse -Force 
 }
 
-#Check if Backupdirs and Destination is available
+#Check if BackupDestinationdirs and Destination is available
 function Check-Dir {
-    Logging "INFO" "Check if BackupDir and Destination exists"
-    if (!(Test-Path $BackupDirs)) {
+    Logging "INFO" "Check if BackupDestinationdir and Destination exists"
+    if (!(Test-Path $BackupDestinationdirs)) {
         return $false
-        Logging "Error" "$BackupDirs does not exist"
+        Logging "Error" "$BackupDestinationdirs does not exist"
     }
     if (!(Test-Path $Destination)) {
         return $false
@@ -146,11 +146,11 @@ Function Make-Backup {
     $colItems=0
     Logging "INFO" "Count all files and create the Top Level Directories"
 
-    foreach ($Backup in $BackupDirs) {
+    foreach ($Backup in $BackupDestinationdirs) {
         $colItems = (Get-ChildItem $Backup -recurse | Where-Object {$_.mode -notmatch "h"} | Measure-Object -property length -sum) 
         $Items=0
         $FilesCount += Get-ChildItem $Backup -Recurse | Where-Object {$_.mode -notmatch "h"}  
-        Copy-Item -Path $Backup -Destination $Backupdir -Force -ErrorAction SilentlyContinue
+        Copy-Item -Path $Backup -Destination $BackupDestinationdir -Force -ErrorAction SilentlyContinue
         $SumMB+=$colItems.Sum.ToString()
         $SumItems+=$colItems.Count
     }
@@ -158,7 +158,7 @@ Function Make-Backup {
     $TotalMB="{0:N2}" -f ($SumMB / 1MB) + " MB of Files"
     Logging "INFO" "There are $SumItems Files with  $TotalMB to copy"
 
-    foreach ($Backup in $BackupDirs) {
+    foreach ($Backup in $BackupDestinationdirs) {
         $Index=$Backup.LastIndexOf("\")
         $SplitBackup=$Backup.substring(0,$Index)
         $Files = Get-ChildItem $Backup -Recurse  | select * | Where-Object {$_.mode -notmatch "h"} | select fullname
@@ -166,7 +166,7 @@ Function Make-Backup {
         foreach ($File in $Files) {
             $restpath = $file.fullname.replace($SplitBackup,"")
             try {
-                Copy-Item  $file.fullname $($Backupdir+$restpath) -Force -ErrorAction SilentlyContinue |Out-Null
+                Copy-Item  $file.fullname $($BackupDestinationdir+$restpath) -Force -ErrorAction SilentlyContinue |Out-Null
                 Logging "INFO" "$file was copied"
             }
             catch {
@@ -175,8 +175,8 @@ Function Make-Backup {
             }
             $Items += (Get-item $file.fullname).Length
             $status = "Copy file {0} of {1} and copied {3} MB of {4} MB: {2}" -f $count,$SumItems,$file.Name,("{0:N2}" -f ($Items / 1MB)).ToString(),("{0:N2}" -f ($SumMB / 1MB)).ToString()
-            $Index=[array]::IndexOf($BackupDirs,$Backup)+1
-            $Text="Copy data Location {0} of {1}" -f $Index ,$BackupDirs.Count
+            $Index=[array]::IndexOf($BackupDestinationdirs,$Backup)+1
+            $Text="Copy data Location {0} of {1}" -f $Index ,$BackupDestinationdirs.Count
             Write-Progress -Activity $Text $status -PercentComplete ($Items / $SumMB*100)  
             if ($File.Attributes -ne "Directory") {$count++}
         }
@@ -196,23 +196,23 @@ Function Make-Backup {
 
 
 
-Create-Backupdir
+Create-BackupDestinationdir
 Logging "INFO" "----------------------"
 Logging "INFO" "Start the Script"
 
-#Check if Backupdir needs to be cleaned and create Backupdir
+#Check if BackupDestinationdir needs to be cleaned and create BackupDestinationdir
 $Count=(Get-ChildItem $Destination | where {$_.Attributes -eq "Directory"}).count
-Logging "INFO" "Check if there are more than $Versions Directories in the Backupdir"
+Logging "INFO" "Check if there are more than $Versions Directories in the BackupDestinationdir"
 
 if ($count -gt $Versions) 
 {
 
-    Delete-Backupdir
+    Delete-BackupDestinationdir
 }
 
 
 $CountZip=(Get-ChildItem $Destination | where {$_.Attributes -eq "Archive" -and $_.Extension -eq ".zip"}).count
-Logging "INFO" "Check if there are more than $Versions Zip in the Backupdir"
+Logging "INFO" "Check if there are more than $Versions Zip in the BackupDestinationdir"
 
 if ($CountZip -gt $Versions) {
 
@@ -249,12 +249,12 @@ if ($CheckDir -eq $false) {
 			$text = "Zippping to "+$Staging
 			Logging "INFO" $text
 			
-			$Zip=$Staging+("\"+$Backupdir.Replace($Staging,'').Replace('\','')+".zip")  
+			$Zip=$Staging+("\"+$BackupDestinationdir.Replace($Staging,'').Replace('\','')+".zip")  
 
 			$text = "Zippping file "+$Zip
 			Logging "INFO" $text
 			
-			Compress-Archive -Path $Backupdir -DestinationPath $Zip -CompressionLevel Optimal -Force
+			Compress-Archive -Path $BackupDestinationdir -DestinationPath $Zip -CompressionLevel Optimal -Force
 			
 			Logging "INFO" "Move Zip to Destination"
 			$text = "Moving to "+$Destination
@@ -276,11 +276,11 @@ if ($CheckDir -eq $false) {
 			$text = "Destination is "+$Destination
 			Logging "INFO" $text
 			
-			$dest=$Destination+("\"+$Backupdir.Replace($Destination,'').Replace('\','')+".zip")  
+			$dest=$Destination+("\"+$BackupDestinationdir.Replace($Destination,'').Replace('\','')+".zip")  
 
 			$text = "Zippping file "+$dest
 			Logging "INFO" $text
-			Compress-Archive -Path $Backupdir -DestinationPath $dest -CompressionLevel Optimal -Force
+			Compress-Archive -Path $BackupDestinationdir -DestinationPath $dest -CompressionLevel Optimal -Force
 		}
 
 
@@ -288,9 +288,9 @@ if ($CheckDir -eq $false) {
         {
             Logging "INFO" "Backupduration $Minutes Minutes and $Seconds Seconds"
 
-            #Remove-Item -Path $BackupDir -Force -Recurse 
-            get-childitem -Path $BackupDir -recurse -Force  | remove-item -Confirm:$false -Recurse
-            get-item -Path $BackupDir   | remove-item -Confirm:$false -Recurse
+            #Remove-Item -Path $BackupDestinationdir -Force -Recurse 
+            get-childitem -Path $BackupDestinationdir -recurse -Force  | remove-item -Confirm:$false -Recurse
+            get-item -Path $BackupDestinationdir   | remove-item -Confirm:$false -Recurse
         }
     }
 }
